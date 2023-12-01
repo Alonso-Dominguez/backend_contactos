06a7e5d4ae4f63286b9c6522bde002f2d891df29
Alonso-Dominguez <122809477+Alonso-Dominguez@users.noreply.github.com>
2023-12-01 17:35:31 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,27 @@
-from fastapi import FastAPI, HTTPException
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
 from fastapi.staticfiles import StaticFiles
-import sqlite3 
-from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+import sqlite3
 
 app = FastAPI()
 
 # Permitimos los origenes para conectarse
 origins = [
-    "http://0.0.0.0:8080", #URL de donde ejecutas tu api local
-    "http://localhost:8080",#URL de donde ejecutas tu api local
-    "http://127.0.0.1:8080",#URL de donde ejecutas tu api local
-    "https://github.com/Alonso-Dominguez/frontend_contactos"
-    #"URL de donde esta tu frontend en heroku",
+    "http://0.0.0.0:8080",  # URL de donde ejecutas tu api local
+    "http://localhost:8080",  # URL de donde ejecutas tu api local
+    "http://127.0.0.1:8080",  # URL de donde ejecutas tu api local
+    # "URL de donde esta tu frontend en heroku",
 ]
 
 # Agregamos las opciones de origenes, credenciales, métodos y headers
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = origins,
-    allow_credentials = True,
-    allow_methods = ["*"],
-    allow_headers = ["*"]
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 # Modelo para la creación de un nuevo contacto
@@ -37,40 +33,45 @@ class ContactoCreate(BaseModel):
     telefono: str
 
 # Conéctate a la base de datos SQLite y crea una tabla para almacenar los contactos si no existe
-conn = sqlite3.connect("contactos.db")
-cursor = conn.cursor()
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS contactos (
-        id_contacto INTEGER PRIMARY KEY,
-        nombre TEXT,
-        primer_apellido TEXT,
-        segundo_apellido TEXT,
-        email TEXT,
-        telefono TEXT
-    )
-''')
-conn.commit()
+def connect_to_database():
+    conn = sqlite3.connect("sql/contactos.db")
+    cursor = conn.cursor()
+    return conn, cursor
+
+def close_database_connection(conn):
+    conn.close()
 
 # Función para obtener todos los contactos
-def obtener_contactos():
+def obtener_contactos(cursor):
     cursor.execute("SELECT * FROM contactos")
     contactos = cursor.fetchall()
     return [dict(zip(["id_contacto", "nombre", "primer_apellido", "segundo_apellido", "email", "telefono"], c)) for c in contactos]
-
-# Resto del código
 
 # Endpoint para agregar un nuevo contacto
 @app.post("/contactos", description="Agregar un nuevo contacto", response_model=dict)
 async def agregar_contacto(contacto: ContactoCreate):
     try:
+        conn, cursor = connect_to_database()
         cursor.execute('''
             INSERT INTO contactos (nombre, primer_apellido, segundo_apellido, email, telefono)
             VALUES (?, ?, ?, ?, ?)
         ''', (contacto.nombre, contacto.primer_apellido, contacto.segundo_apellido, contacto.email, contacto.telefono))
         conn.commit()
+        close_database_connection(conn)
         return contacto.dict()
     except Exception as e:
         raise HTTPException(status_code=500, detail="Error al agregar el contacto")
+
+# Endpoint para obtener todos los contactos
+@app.get("/contactos", description="Obtener todos los contactos", response_model=list[dict])
+async def get_contactos():
+    try:
+        conn, cursor = connect_to_database()
+        result = obtener_contactos(cursor)
+        close_database_connection(conn)
+        return result
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Error al obtener los contactos")
 
 # Endpoint para obtener todos los contactos
 @app.get("/contactos", description="Obtener todos los contactos", response_model=list[dict])
@@ -85,6 +86,7 @@ async def get_contactos():
 @app.patch("/contactos/{contacto_id}", description="Actualizar un contacto por su ID", response_model=dict)
 async def actualizar_contacto(contacto_id: int, contacto: ContactoCreate):
     try:
+        conn, cursor = connect_to_database()
         cursor.execute('''
             UPDATE contactos
             SET nombre = ?, primer_apellido = ?, segundo_apellido = ?, email = ?, telefono = ?
@@ -93,39 +95,49 @@ async def actualizar_contacto(contacto_id: int, contacto: ContactoCreate):
         conn.commit()
 
         if cursor.rowcount == 0:
+            close_database_connection(conn)
             raise HTTPException(status_code=404, detail="Contacto no encontrado")
         
+        close_database_connection(conn)
         return { "id_contacto": contacto_id, **contacto.dict() }
 
-    except Exception as e:
+    except sqlite3.Error as e:
         raise HTTPException(status_code=500, detail="Error al actualizar el contacto")
 
 # Endpoint para borrar un contacto por id_contacto
 @app.delete("/contactos/{contacto_id}", description="Borrar un contacto por su ID", response_model=dict)
 async def borrar_contacto(contacto_id: int):
     try:
+        conn, cursor = connect_to_database()
+
         cursor.execute("SELECT * FROM contactos WHERE id_contacto = ?", (contacto_id,))
         contacto = cursor.fetchone()
 
         if not contacto:
+            close_database_connection(conn)
             raise HTTPException(status_code=404, detail="Contacto no encontrado")
 
         cursor.execute("DELETE FROM contactos WHERE id_contacto = ?", (contacto_id,))
         conn.commit()
 
+        close_database_connection(conn)
+
         return { "id_contacto": contacto_id, **dict(zip(["nombre", "primer_apellido", "segundo_apellido", "email", "telefono"], contacto)) }
 
-    except Exception as e:
+    except sqlite3.Error as e:
         raise HTTPException(status_code=500, detail="Error al borrar el contacto")
+
 
 # Endpoint para buscar un contacto por email 
 @app.get("/contactos/buscar", description="Buscar contactos por email", response_model=list[dict])
 async def buscar_contactos_por_email(email: str):
     try:
         print(f'Búsqueda de contactos por email: {email}')
+        conn, cursor = connect_to_database()
         cursor.execute('SELECT * FROM contactos WHERE email LIKE ?', ('%' + email + '%',))
         contactos = cursor.fetchall()
         print(f'Resultados de la búsqueda: {contactos}')
+        close_database_connection(conn)
         return [dict(zip(["id_contacto", "nombre", "primer_apellido", "segundo_apellido", "email", "telefono"], c)) for c in contactos]
     except Exception as e:
         print(f'Error al buscar contactos por email: {e}')
